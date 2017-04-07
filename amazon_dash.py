f577bf90df08c028ff90cc6b84ce2fc35cb41e7d
masterAndrey <filbert@yandex.ru>
2017-04-07 08:56:33 +0300

    buttons description in external file, show unknown macs only once

--- amazon_dash.py
+++ amazon_dash.py
@@ -6,18 +6,33 @@ Sniff for ARP traffic
 
 from scapy.all import *
 from google_sheet import Sheet
+import json
+
+
+BUTTONS_FILE_NAME = 'amazone-dash-private/buttons.json'
+
+buttons = {}
+seen_macs = set()
+
+
+def load_buttons():
+    """ Load known buttons """
+    with open(BUTTONS_FILE_NAME, 'r') as buttons_file:
+        buttons = json.loads(buttons_file.read())
+    return buttons
+
 
 def arp_handler(pkt):
     """ Handles sniffed ARP requests """
-    BUTTONS = {
-        '34:d2:70:a4:e0:50': 'white'
-    }
     if pkt.haslayer(ARP):
         if pkt[ARP].op == 1: #who-has request
-            if pkt[ARP].hwsrc in BUTTONS:
-                trigger(BUTTONS[pkt[ARP].hwsrc])
-            # else:
-            #     print('ARP request from unknown MAC:{}'.format(pkt[ARP].hwsrc))
+            if pkt[ARP].hwsrc in buttons:
+                trigger(buttons[pkt[ARP].hwsrc])
+            else:
+                if pkt[ARP].hwsrc not in seen_macs:
+                    print('ARP request from unknown MAC {}'.format(pkt[ARP].hwsrc))
+                    seen_macs.add(pkt[ARP].hwsrc)
+
 
 def trigger(button):
     """ Button press action """
@@ -28,7 +43,9 @@ def trigger(button):
 
 
 def main():
-    print('amazon_dash started')
+    global buttons
+    buttons = load_buttons()
+    print('amazon_dash started, loaded {} buttons'.format(len(buttons)))
     sniff(prn=arp_handler, filter="arp", store=0)
 
 main()
