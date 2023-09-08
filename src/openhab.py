83b88c34a7c405119f3fd8627d82322e1bfe19db
andgineer <Andrei_Sorokin2@epam.com>
2023-09-08 06:20:24 +0200

    https://github.com/andgineer/docker-amazon-dash-button-hack/issues/19 mypy open_hub

--- src/openhab.py
+++ src/openhab.py
@@ -1,39 +1,39 @@
-"""
-Action for OpenHAB item
+"""Action for OpenHAB item.
+
 http://demo.openhab.org:8080/doc/index.html
 """
 
 import json
+from typing import Any, Dict
 
 import requests
 
 
 class OpenHab:
-    def __init__(self, settings):
+    """Action for OpenHAB item."""
+
+    def __init__(self, settings: Dict[str, Any]) -> None:
+        """Init."""
         self.settings = settings
 
-    def press(self, action_params):
-        """
-        Gets current item state and changes it to opposite status.
-        """
-        base_url = "{path}/items/{item}".format(
-            path=action_params["path"],
-            item=action_params["item"],
-        )
+    def press(self, action_params: Dict[str, Any]) -> None:
+        """Get current item state and changes it to opposite status."""
+        base_url = f"{action_params['path']}/items/{action_params['item']}"
         commands = action_params["command"].upper().split(";")
         if len(commands) != 2:
             print(
-                '\nWrong "command" setting in openhab action. Should be two openHAB commands separated by ";" ("ON;OFF" or "UP;DOWN"). Button press will switch between them.'
+                '\nWrong "command" setting in openhab action. '
+                'Should be two openHAB commands separated by ";" ("ON;OFF" or "UP;DOWN"). '
+                'Button press will switch between them.'
             )
 
-        state = requests.get(base_url + "/state", headers={"content-type": "application/json"})
+        state = requests.get(f"{base_url}/state", headers={"content-type": "application/json"}, timeout=5)
         try:
             current_idx = commands.index(state.text.upper())
         except ValueError:
             print(
-                'Item {} now in state {}. But in "command" settings ({}) there is no such state.'.format(
-                    action_params["item"], state.text, action_params["command"]
-                )
+                f'Item {action_params["item"]} now in state {state.text}. '
+                f'But in "command" settings ({action_params["command"]}) there is no such state.'
             )
             return
 
@@ -42,4 +42,5 @@ class OpenHab:
             base_url,
             data=json.dumps(commands[command_idx]),
             headers={"content-type": "application/json"},
+            timeout=5,
         )
