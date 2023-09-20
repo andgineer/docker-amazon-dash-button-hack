7ce9342850b661b88aa00f098f5e02ada672409b
andgineer <Andrei_Sorokin2@epam.com>
2023-09-20 20:01:26 +0200

    https://github.com/andgineer/docker-amazon-dash-button-hack/issues/12 typo

--- scripts/compile_requirements.sh
+++ scripts/compile_requirements.sh
@@ -6,6 +6,6 @@
 rm -f requirements.txt
 rm -f requirements.dev.txt
 
-# we should process requirements.in first because it's included in requirements.dev.in
+# we should process requirements.in first because resulting requirements.txt included in requirements.dev.in
 pip-compile requirements.in
 pip-compile requirements.dev.in
