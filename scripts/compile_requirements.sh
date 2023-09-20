6505b8d58fced8ec730c2145dee0c47e93909bae
andgineer <Andrei_Sorokin2@epam.com>
2023-09-20 17:11:21 +0200

    https://github.com/andgineer/docker-amazon-dash-button-hack/issues/12 requirements processing order

--- scripts/compile_requirements.sh
+++ scripts/compile_requirements.sh
@@ -6,5 +6,6 @@
 rm -f requirements.txt
 rm -f requirements.dev.txt
 
+# we should process requirements.in first because it's included in requirements.dev.in
 pip-compile requirements.in
 pip-compile requirements.dev.in
