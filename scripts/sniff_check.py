89a1cc8da2b6a441ae1216d37797e645b8104d87
andgineer <Andrei_Sorokin2@epam.com>
2023-09-21 06:46:01 +0200

    https://github.com/andgineer/docker-amazon-dash-button-hack/issues/26 sniff_check now ca be safely imported

--- scripts/sniff_check.py
+++ scripts/sniff_check.py
@@ -3,7 +3,8 @@ import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../src"))
 
-from amazon_dash import AmazonDash
+if __name__ == "__main__":
+    from amazon_dash import AmazonDash
 
-amazon_dash = AmazonDash()
-amazon_dash.run()
+    amazon_dash = AmazonDash()
+    amazon_dash.run()
