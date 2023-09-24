f126bcc0cdeaa32d95db4d88c2fe29967c1cf122
andgineer <Andrei_Sorokin2@epam.com>
2023-09-24 07:18:00 +0200

    https://github.com/andgineer/docker-amazon-dash-button-hack/issues/11 cleanup root or the project

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,12 @@ sys.modules["scapy"] = Mock()
 sys.modules["scapy.layers.dhcp"] = Mock()
 sys.modules["scapy.layers.l2"] = Mock()
 
+# Point sys.path to our sources before importing anything from them
+import os.path
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../src"))
+
 # We need to import amazon_dash after mocking scapy
 from amazon_dash import AmazonDash
 
