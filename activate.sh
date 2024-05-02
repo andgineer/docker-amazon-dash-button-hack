51671e763ad1c2bc20c06d26f2b02c841d85380b
Andrei Sorokin <Andrei_Sorokin2@epam.com>
2024-05-02 06:41:43 +0200

    python 3.12

--- activate.sh
+++ activate.sh
@@ -4,7 +4,7 @@
 #
 
 VENV_FOLDER=".venv"
-PYTHON="python3.11"  # sync with python-version: '3.11' in .github/workflows/docs.yml,static.yml
+PYTHON="python3.12"  # sync with .github/workflows/docs.yml,static.yml
 
 RED='\033[1;31m'
 GREEN='\033[1;32m'
